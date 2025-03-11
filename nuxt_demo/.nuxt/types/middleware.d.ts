1de5c16a15b7b193e9c16531e30dbfafddbf42b0
hollow <1315742953@qq.com>
2025-03-11 18:07:03 +0800

    git无视.nuxt和.output

--- nuxt_demo/.nuxt/types/middleware.d.ts
+++
@@ -1,7 +0,0 @@
-import type { NavigationGuard } from 'vue-router'
-export type MiddlewareKey = never
-declare module "../../node_modules/.pnpm/nuxt@3.16.0_@parcel+watcher@2.5.1_@types+node@22.13.10_db0@0.3.1_eslint@9.22.0_jiti@2.4.2__io_qvuatha57ukqh7gg4mt7eeyqci/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
-  }
-}
--- nuxt_demo/.output/public/_nuxt/reBHNhuI.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,t,v as n}from"./CbvlugHT.js";const o={};function c(r,s,a,_,p,d){return n(),t("div",null,"index")}const f=e(o,[["render",c]]);export{f as default};
